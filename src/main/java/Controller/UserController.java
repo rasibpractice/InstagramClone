d16d92a2d58ffb36959b54a221335cb9324278e6
sakhawath <sakhawathhossain162@gmail.com>
2021-05-18 22:29:02 +0600

    Post and Get method created for User Controller class

---
+++ src/main/java/Controller/UserController.java
@@ -0,0 +1,26 @@
+package Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import Model.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	@PostMapping("")
+	public void addUser(User user) {
+		
+	}
+	
+	@GetMapping("/{userid}")
+	public User getUser(@PathVariable("userid") String userId) {
+		return new User();
+	}
+
+}
